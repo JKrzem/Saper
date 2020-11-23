87ef9fb610fb60b9986031784d73547a42b1e3ec
Kuba <kubakrzem17@gmail.com>
2020-11-23 21:38:04 +0100

    created basic UI

---
+++ js/UI.js
@@ -0,0 +1,16 @@
+export class UI {
+    UiSelectors = {
+        board: '[data-board]',
+        cell: '[data-cell]',
+    }
+
+    getElement(selector)
+    {
+        return document.querySelector(selector);
+    }
+
+    getElements(selector)
+    {
+        return document.querySelectorAll(selector);
+    }
+}
